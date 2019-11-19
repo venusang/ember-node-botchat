20521544bd0605d7913cd498cd724fe9c4d23cc4
Venus Ang <venusang24@gmail.com>
2019-11-19 14:48:56 -0800

    wip - have Phase 1 fulfilled but realize I think I need to use Pusher Chatkit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Pusher = require("pusher");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Ensure that your pusher credential are properly set in the .env file
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  forceTLS: true
+});
+
+const dialogFlowURL = "https://api.dialogflow.com/v1/query?v=20150910";
+const token = process.env.DEVELOPER_ACCESS_TOKEN;
+
+app.set("port", process.env.PORT || 3000);
+//https://nodejs.org/docs/v0.4.12/api/http.html#response.writeHead
+//https://expressjs.com/en/api.html#setHeaders
+//https://stackoverflow.com/questions/23751914/how-can-i-set-response-header-on-express-js-assets
+//response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+//https://github.com/apache/couchdb/issues/2221
+app.post("/dialogue", (req, res) => {
+  const data = {
+    query: req.body.text,
+    lang: "en",
+    sessionId: "1234567890!@#$%^&*()"
+  };
+
+  axios
+    .post(`${dialogFlowURL}`, data, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(response => {
+      const responseData = {
+        query: data.query,
+        speech: response.data.result.fulfillment.speech
+      };
+      pusher.trigger("bot", "bot-response", responseData);
+    });
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Listening on " + app.get("port"));
+});
